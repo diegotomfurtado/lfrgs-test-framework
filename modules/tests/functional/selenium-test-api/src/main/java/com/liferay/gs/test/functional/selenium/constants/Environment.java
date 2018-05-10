12eaad5b2fa5e65c9033e3464b7f743af5483aa6
arbetts <andrew.betts@liferay.com>
2018-05-10 08:00:08 -0700

    seleniums platform class already give the correct OS

--- modules/tests/functional/selenium-test-api/src/main/java/com/liferay/gs/test/functional/selenium/constants/Environment.java
+++ modules/tests/functional/selenium-test-api/src/main/java/com/liferay/gs/test/functional/selenium/constants/Environment.java
@@ -5,30 +5,6 @@ package com.liferay.gs.test.functional.selenium.constants;
  */
 public class Environment {
 
-	public enum OSName {
-		WINDOWS, LINUX, MAC
-	}
-
-	public static OSName getOSName() {
-		String osName = System.getProperty("os.name");
-
-		if (osName.contains("WIN") ||
-			osName.contains("win") ||
-			osName.contains("Win")) {
-
-			return OSName.WINDOWS;
-		}
-
-		if (osName.contains("MAC") ||
-			osName.contains("Mac") ||
-			osName.contains("mac")) {
-
-			return OSName.MAC;
-		}
-
-		return OSName.LINUX;
-	}
-
 	public static boolean is64Bit() {
 		String bit = System.getProperty("sun.arch.data.model");
 
