16ededb75404b8441f89cb11ba33de057b4c7a57
arbetts <andrew.betts@liferay.com>
2018-05-11 20:35:34 -0700

    add functional tests for actions

--- modules/tests/functional/selenium-test-actions/src/test/java/com/liferay/gs/test/functional/selenium/actions/LiferayUserActionsTest.java
+++ modules/tests/functional/selenium-test-actions/src/test/java/com/liferay/gs/test/functional/selenium/actions/LiferayUserActionsTest.java
@@ -1,7 +1,61 @@
 package com.liferay.gs.test.functional.selenium.actions;
 
+import com.liferay.gs.test.functional.selenium.properties.SeleniumProperties;
+import com.liferay.gs.test.functional.selenium.properties.SeleniumPropertyKeys;
+import com.liferay.gs.test.functional.selenium.runner.Browsers;
+import com.liferay.gs.test.functional.selenium.runner.LiferaySeleniumTestRunner;
+import com.liferay.gs.test.functional.selenium.runner.WebDriverField;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.BrowserType;
+
 /**
  * @author Andrew Betts
  */
+@Browsers({BrowserType.CHROME})
+@RunWith(LiferaySeleniumTestRunner.class)
 public class LiferayUserActionsTest {
+
+	@Test
+	public void testSignInSignOut() {
+		LiferayUserActions liferayUserActions = new LiferayUserActions();
+		BaseWebDriverActions baseWebDriverActions = new BaseWebDriverActions();
+
+		By userIcon = By.cssSelector(
+			".user-icon-color-6.user-icon.user-icon-default");
+
+		WebElement userIconElement = baseWebDriverActions.fetchWebElement(
+			userIcon, _webDriver);
+
+		Assert.assertNull(userIconElement);
+
+		String login = SeleniumProperties.get(
+			SeleniumPropertyKeys.TEST_DEFAULT_USER_LOGIN);
+		String password = SeleniumProperties.get(
+			SeleniumPropertyKeys.TEST_DEFAULT_USER_PASSWWORD);
+
+		liferayUserActions.signIn(login, password, _webDriver);
+
+		userIconElement = baseWebDriverActions.fetchWebElement(
+			userIcon, _webDriver);
+
+		Assert.assertNotNull(userIconElement);
+
+		liferayUserActions.signOut(_webDriver);
+
+		userIconElement = baseWebDriverActions.fetchWebElement(
+			userIcon, _webDriver);
+
+		Assert.assertNull(userIconElement);
+	}
+
+	@WebDriverField
+	private WebDriver _webDriver;
+
 }
--- modules/tests/functional/selenium-test-actions/src/test/java/com/liferay/gs/test/functional/selenium/actions/ProductNavigationMenuActionsTest.java
+++ modules/tests/functional/selenium-test-actions/src/test/java/com/liferay/gs/test/functional/selenium/actions/ProductNavigationMenuActionsTest.java
@@ -1,11 +1,17 @@
 package com.liferay.gs.test.functional.selenium.actions;
 
+import com.liferay.gs.test.functional.selenium.actions.rule.SignedInTestRule;
 import com.liferay.gs.test.functional.selenium.runner.Browsers;
 import com.liferay.gs.test.functional.selenium.runner.LiferaySeleniumTestRunner;
 import com.liferay.gs.test.functional.selenium.runner.WebDriverField;
+
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.BrowserType;
 
 /**
@@ -15,13 +21,29 @@ import org.openqa.selenium.remote.BrowserType;
 @RunWith(LiferaySeleniumTestRunner.class)
 public class ProductNavigationMenuActionsTest {
 
+	@Rule
+	public SignedInTestRule signedInTestRule = new SignedInTestRule();
+
 	@Test
 	public void testAddPortletOnScreen() {
+		LiferayPortletActions alertsPortletActions = new LiferayPortletActions(
+			LiferayPortletIds.AlertsPortlet);
+
 		ProductNavigationMenuActions productNavigationMenuActions =
 			new ProductNavigationMenuActions();
 
 		productNavigationMenuActions.addPortletOnScreen(
-			LiferayPortletIds.AlertsPortlet, "i", _webDriver);
+			"Alerts", LiferayPortletIds.AlertsPortlet, "column-1", _webDriver);
+
+		WebElement alertsPortlet = alertsPortletActions.getPortlet(_webDriver);
+
+		Assert.assertNotNull(alertsPortlet);
+
+		alertsPortletActions.remove(_webDriver);
+
+		alertsPortlet = alertsPortletActions.getPortlet(_webDriver);
+
+		Assert.assertNull(alertsPortlet);
 	}
 
 	@WebDriverField
