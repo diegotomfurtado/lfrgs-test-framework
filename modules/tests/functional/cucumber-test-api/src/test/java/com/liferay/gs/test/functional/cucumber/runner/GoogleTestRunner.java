2f05912ef5dad767d81b08a37fa8a281124b433c
arbetts <andrew.betts@liferay.com>
2018-05-15 06:59:10 -0700

    update cucumbder-test-api googleTestRunner to point to the correct package

--- modules/tests/functional/cucumber-test-api/src/test/java/com/liferay/gs/test/functional/cucumber/runner/GoogleTestRunner.java
+++ modules/tests/functional/cucumber-test-api/src/test/java/com/liferay/gs/test/functional/cucumber/runner/GoogleTestRunner.java
@@ -3,7 +3,9 @@ package com.liferay.gs.test.functional.cucumber.runner;
 import com.liferay.gs.test.functional.cucumber.LiferayCucumberRunner;
 import com.liferay.gs.test.functional.selenium.constants.BrowserDrivers;
 import com.liferay.gs.test.functional.selenium.runner.Browsers;
+
 import cucumber.api.CucumberOptions;
+
 import org.junit.runner.RunWith;
 
 /**
@@ -12,7 +14,7 @@ import org.junit.runner.RunWith;
 @Browsers({BrowserDrivers.BROWSER_CHROME})
 @CucumberOptions(
 	features = {"src/test/resources/features/Google.feature"},
-	glue = {"com.liferay.functional.test.cucumber.step"}
+	glue = {"com.liferay.gs.test.functional.cucumber.step"}
 )
 @RunWith(LiferayCucumberRunner.class)
 public class GoogleTestRunner {
