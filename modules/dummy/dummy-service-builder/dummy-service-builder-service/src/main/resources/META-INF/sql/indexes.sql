98b55f5b0c8a1dfe9c56873d8f17fafeb01e2976
“diego.furtado” <diegotomfurtado@gmail.com>
2019-04-01 21:48:00 -0300

    trying to work with Jenkinsfile

--- modules/dummy/dummy-service-builder/dummy-service-builder-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,4 +0,0 @@
-create unique index IX_21DB76CC on TEST_Dummy (field1[$COLUMN_LENGTH:75$]);
-create index IX_21DB7A8D on TEST_Dummy (field2);
-create index IX_1A828355 on TEST_Dummy (uuid_[$COLUMN_LENGTH:75$], companyId);
-create unique index IX_200DDB17 on TEST_Dummy (uuid_[$COLUMN_LENGTH:75$], groupId);
